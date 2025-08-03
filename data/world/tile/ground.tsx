a1ceb142788c039e5110101b5a3d8bb0da559535
kkkunny <1401289859@qq.com>
2025-08-03 19:12:57 +0800

    feat: 海水动态效果

--- data/world/tile/ground.tsx
+++ data/world/tile/ground.tsx
@@ -1,4 +1,16 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="ground" tilewidth="16" tileheight="16" tilecount="480" columns="24">
  <image source="ground.png" trans="000000" width="384" height="320"/>
+ <tile id="24">
+  <animation>
+   <frame tileid="24" duration="300"/>
+   <frame tileid="25" duration="300"/>
+   <frame tileid="26" duration="300"/>
+   <frame tileid="27" duration="300"/>
+   <frame tileid="28" duration="300"/>
+   <frame tileid="29" duration="300"/>
+   <frame tileid="30" duration="300"/>
+   <frame tileid="31" duration="300"/>
+  </animation>
+ </tile>
 </tileset>
